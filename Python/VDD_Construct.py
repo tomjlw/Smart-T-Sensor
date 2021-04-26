c43f55291cfd05ea2336faae981e3f4e2f7cc3ca
Liwen Jiang <tomjlw123@gmail.com>
2021-04-25 20:19:33 -0500

    Update and rename VDD_Construct to VDD_Construct.py

--- Python/VDD_Construct.py
+++ Python/VDD_Construct.py
@@ -3,8 +3,11 @@ import matplotlib.pyplot as plt
 import pandas as pd 
 import csv
 import math
+from sklearn.metrics import r2_score
+from sklearn.linear_model import LinearRegression
 
 raw_data = []
+R2 = []
 N_TK = 21
 N_VDD = 21
 VDD_min = 0.2
@@ -12,7 +15,7 @@ VDD_max = 0.4
 TK_min = 273
 TK_max = 373
 TT_VDD = 0.26
-diff_VDD = 0.00 # the VDD variation values across corners
+diff_VDD = 0.03 # the VDD variation values across corners
 file_name = "VDD_Construct.csv"
 
 
@@ -41,18 +44,27 @@ VDD_idx = [round((element-VDD_min)/(VDD_list[1]-VDD_list[0]))
 constructed_data = np.array([raw_data[idx, :, corner_idx] for corner_idx, idx in enumerate(VDD_idx)]).T # construct new dataset
 
 # Translation of original matlab script calculating  overal sigma
-Process = np.around(np.array([TK_list*np.log(constructed_data[:, i]).T for i in range(0, constructed_data.shape[1])]).T, decimals=0)
+Process = np.around(np.array([TK_list*np.log(constructed_data[:, i]).T for i in range(0, constructed_data.shape[1])]).T, decimals=2)
 P2 = np.array([np.polyfit(TK_list, Process[:, i], 1)[1] for i in range(0, constructed_data.shape[1])])
 P1 = np.array([np.polyfit(TK_list, Process[:, i], 1)[0] for i in range(0, constructed_data.shape[1])])
 Meas = np.array([np.divide(P2[i],np.log(constructed_data[:, i])-P1[i]) for i in range(0, constructed_data.shape[1])]).T
 error = Meas - np.array(TK_repeat)
 
-FF = np.array(np.polyfit(TK_list, error, 3))
+#FF = np.array([np.polyfit(TK_list, error[:, i], 3) for i in range(0, constructed_data.shape[1])]).T
+FF = np.polyfit(TK_list, error, 3)
 error_after_fitting = error - np.polyval(FF, TK_repeat)
 
 std = np.std(error_after_fitting, axis=1)
 average = np.mean(error_after_fitting, axis=1) 
 overall_sigma = np.max(average+3*std)-np.min(average-3*std)
 
-print(std)
 print(overall_sigma)
+
+# Calculate R2 value
+linear_model = LinearRegression()
+
+for i in range(0, constructed_data.shape[1]):
+	linear_model.fit(TK_list.reshape(-1, 1), Process[:, i])
+	R2.append(linear_model.score(TK_list.reshape(-1, 1), Process[:, i]))
+
+print(R2)
